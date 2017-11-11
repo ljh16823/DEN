e0bf4e4985fd4482327d3f999c694021e93666c9
ljh16823 <ljh16823@naver.com>
2017-11-11 18:30:05 +0900

    Se1

--- app/src/main/java/com/example/ljh16/den/MainActivity.java
+++ app/src/main/java/com/example/ljh16/den/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.ljh16.den;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.view.View;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton a = (ImageButton) findViewById(R.id.imageButton);
+        ImageButton b = (ImageButton) findViewById(R.id.imageButton2);
+        ImageButton c = (ImageButton) findViewById(R.id.imageButton3);
+        ImageButton d = (ImageButton) findViewById(R.id.imageButton4);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mapsuchintent = new Intent(MainActivity.this, mapsuch.class);
+                MainActivity.this.startActivity(mapsuchintent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ljh16/den/mapsuch.java
@@ -0,0 +1,34 @@
+package com.example.ljh16.den;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+public class mapsuch extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mapsuch_two);
+        ImageButton igbutton = (ImageButton) findViewById(R.id.imageButton);
+        ImageButton igbutton2 = (ImageButton) findViewById(R.id.imageButton2);
+        ImageButton igbutton3 = (ImageButton) findViewById(R.id.imageButton3);
+        ImageButton igbutton4 = (ImageButton) findViewById(R.id.imageButton4);
+        ImageButton igbutton5 = (ImageButton) findViewById(R.id.imageButton5);
+        ImageButton igbutton6 = (ImageButton) findViewById(R.id.imageButton6);
+        ImageButton igbutton7 = (ImageButton) findViewById(R.id.imageButton7);
+        ImageButton igbutton8 = (ImageButton) findViewById(R.id.imageButton8);
+        ImageButton igbutton9 = (ImageButton) findViewById(R.id.imageButton9);
+        ImageButton igbutton10 = (ImageButton) findViewById(R.id.imageButton10);
+        ImageButton igbutton11 = (ImageButton) findViewById(R.id.imageButton11);
+        ImageButton igbutton12 = (ImageButton) findViewById(R.id.imageButton12);
+        ImageButton igbutton13 = (ImageButton) findViewById(R.id.imageButton13);
+        ImageButton igbutton14 = (ImageButton) findViewById(R.id.imageButton14);
+        ImageButton igbutton15 = (ImageButton) findViewById(R.id.imageButton15);
+
+
+    }
+}
+
