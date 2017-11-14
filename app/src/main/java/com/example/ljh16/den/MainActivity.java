55c1f3a26768e74a75b10786266725cb7f719cdd
pshboom <33223592+pshboom@users.noreply.github.com>
2017-11-14 21:16:48 +0900

    박상현

--- app/src/main/java/com/example/ljh16/den/MainActivity.java
+++ app/src/main/java/com/example/ljh16/den/MainActivity.java
@@ -28,9 +28,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent imageBu2Intent = new Intent(MainActivity.this, searchbybuilding.class);
-                Intent imageBu3Intent = new Intent(MainActivity.this, Adapter.class);
                 MainActivity.this.startActivity(imageBu2Intent);
-                MainActivity.this.startActivity(imageBu3Intent);
             }
         });
     }
