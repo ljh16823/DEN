845b08093d91e3770a64d400b8b1d88b7b165a9e
pshboom <33223592+pshboom@users.noreply.github.com>
2017-11-15 09:26:03 +0900

    박상현

--- app/src/main/java/com/example/ljh16/den/slideractivity.java
+++ app/src/main/java/com/example/ljh16/den/slideractivity.java
@@ -10,14 +10,13 @@ import android.view.View;
 /**
  * Created by pshbo on 2017-11-14.
  */
+public class slideractivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.slider);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
 
-    public class slideractivity extends Activity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.slider);
-            TextView tV = (TextView) findViewById(R.id.textView);
-            ImageView iV = (ImageView) findViewById(R.id.imageView);
-
-        }
     }
+}
