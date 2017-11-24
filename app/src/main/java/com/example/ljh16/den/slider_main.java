b7b89f8abf6af1ed0f4cef14c8d9f7606790fe9e
pshboom <33223592+pshboom@users.noreply.github.com>
2017-11-24 18:40:54 +0900

    박상현

--- app/src/main/java/com/example/ljh16/den/slider_main.java
+++ app/src/main/java/com/example/ljh16/den/slider_main.java
@@ -3,9 +3,10 @@ package com.example.ljh16.den;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class slider_main extends Activity {
+public class slider_main extends AppCompatActivity {
     adapter adapter;
     ViewPager viewPager;
     @Override
