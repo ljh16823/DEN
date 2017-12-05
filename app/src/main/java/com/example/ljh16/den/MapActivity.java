e421dbfa77ecf31ffe5b2d26a3d9d024778017f2
ljh16823 <ljh16823@naver.com>
2017-12-05 23:26:33 +0900

    구

--- app/src/main/java/com/example/ljh16/den/MapActivity.java
+++ app/src/main/java/com/example/ljh16/den/MapActivity.java
@@ -12,7 +12,7 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.activity_map);
-        Button Bu = findViewById(R.id.Bu);
+        Button Bu = findViewById(R.id.기초과학관);
         Bu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -20,7 +20,7 @@ public class MapActivity extends AppCompatActivity {
                 startActivity(BuIntent);
             }
         });
-        Button Bu2 =  findViewById(R.id.Bu2);
+        Button Bu2 =  findViewById(R.id.공학관);
         Bu2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,7 +28,7 @@ public class MapActivity extends AppCompatActivity {
                 startActivity(Bu2Intent);
             }
         });
-        Button Bu3 = findViewById(R.id.Bu3);
+        Button Bu3 = findViewById(R.id.응용과학관);
         Bu3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,7 +36,7 @@ public class MapActivity extends AppCompatActivity {
                 startActivity(Bu3Intent);
             }
         });
-        Button Bu4 = findViewById(R.id.Bu4);
+        Button Bu4 = findViewById(R.id.공학실험관);
         Bu4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,7 +44,7 @@ public class MapActivity extends AppCompatActivity {
                 startActivity(Bu4Intent);
             }
         });
-        Button Bu5 = findViewById(R.id.Bu5);
+        Button Bu5 = findViewById(R.id.학생회관);
         Bu5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,7 +52,7 @@ public class MapActivity extends AppCompatActivity {
                 startActivity(Bu5Intent);
             }
         });
-        Button Bu6 = findViewById(R.id.Bu6);
+        Button Bu6 = findViewById(R.id.창의학습관);
         Bu6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,7 +60,7 @@ public class MapActivity extends AppCompatActivity {
                 startActivity(Bu6Intent);
             }
         });
-        Button Bu7 = findViewById(R.id.Bu7);
+        Button Bu7 = findViewById(R.id.창학관);
         Bu7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,7 +68,7 @@ public class MapActivity extends AppCompatActivity {
                 startActivity(Bu7Intent);
             }
         });
-        Button Bu8 = findViewById(R.id.Bu8);
+        Button Bu8 = findViewById(R.id.MACC센터);
         Bu8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -76,7 +76,7 @@ public class MapActivity extends AppCompatActivity {
                 startActivity(Bu8Intent);
             }
         });
-        Button Bu9 = findViewById(R.id.Bu9);
+        Button Bu9 = findViewById(R.id.렉처홀);
         Bu9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -84,7 +84,7 @@ public class MapActivity extends AppCompatActivity {
                 startActivity(Bu9Intent);
             }
         });
-        Button Bu10 = findViewById(R.id.Bu10);
+        Button Bu10 = findViewById(R.id.지산도서관);
         Bu10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -101,7 +101,7 @@ public class MapActivity extends AppCompatActivity {
                 startActivity(Bu11Intent);
             }
         });
-        Button Bu12 = findViewById(R.id.Bu12);
+        Button Bu12 = findViewById(R.id.혜화문화관);
         Bu12.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -110,7 +110,7 @@ public class MapActivity extends AppCompatActivity {
 
             }
         });
-        Button Bu13 = findViewById(R.id.Bu13);
+        Button Bu13 = findViewById(R.id.인문사회관);
         Bu13.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -119,7 +119,7 @@ public class MapActivity extends AppCompatActivity {
 
             }
         });
-        Button Bu14 = findViewById(R.id.Bu14);
+        Button Bu14 = findViewById(R.id.문무관);
         Bu14.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -128,7 +128,7 @@ public class MapActivity extends AppCompatActivity {
 
             }
         });
-        Button Bu15 = findViewById(R.id.Bu15);
+        Button Bu15 = findViewById(R.id.한의학관);
         Bu15.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -137,7 +137,7 @@ public class MapActivity extends AppCompatActivity {
 
             }
         });
-        Button Bu16 = findViewById(R.id.Bu16);
+        Button Bu16 = findViewById(R.id.산학협력관);
         Bu16.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/ljh16/den/TtimeActivity.java
+++ app/src/main/java/com/example/ljh16/den/TtimeActivity.java
@@ -1,5 +1,6 @@
 package com.example.ljh16.den;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,51 +14,52 @@ public class TtimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ttime);
-        CheckBox cbMon1 = (CheckBox) findViewById(R.id.Mon1);
-        CheckBox cbMon2 = (CheckBox) findViewById(R.id.Mon2);
-        CheckBox cbMon3 = (CheckBox) findViewById(R.id.Mon3);
-        CheckBox cbMon4 = (CheckBox) findViewById(R.id.Mon4);
-        CheckBox cbMon5 = (CheckBox) findViewById(R.id.Mon5);
-        CheckBox cbMon6 = (CheckBox) findViewById(R.id.Mon6);
-        CheckBox cbMon7 = (CheckBox) findViewById(R.id.Mon7);
-        CheckBox cbMon8 = (CheckBox) findViewById(R.id.Mon8);
-        CheckBox cbMon9 = (CheckBox) findViewById(R.id.Mon9);
-        CheckBox cbTues1 = (CheckBox) findViewById(R.id.Tues1);
-        CheckBox cbTues2 = (CheckBox) findViewById(R.id.Tues2);
-        CheckBox cbTues3 = (CheckBox) findViewById(R.id.Tues3);
-        CheckBox cbTues4 = (CheckBox) findViewById(R.id.Tues4);
-        CheckBox cbTues5 = (CheckBox) findViewById(R.id.Tues5);
-        CheckBox cbTues6 = (CheckBox) findViewById(R.id.Tues6);
-        CheckBox cbTues7 = (CheckBox) findViewById(R.id.Tues7);
-        CheckBox cbTues8 = (CheckBox) findViewById(R.id.Tues8);
-        CheckBox cbTues9 = (CheckBox) findViewById(R.id.Tues9);
-        CheckBox cbWednes = (CheckBox) findViewById(R.id.Wednes1);
-        CheckBox cbWednes2 = (CheckBox) findViewById(R.id.Wednes2);
-        CheckBox cbWednes3 = (CheckBox) findViewById(R.id.Wednes3);
-        CheckBox cbWednes4 = (CheckBox) findViewById(R.id.Wednes4);
-        CheckBox cbWednes5 = (CheckBox) findViewById(R.id.Wednes5);
-        CheckBox cbWednes6 = (CheckBox) findViewById(R.id.Wednes6);
-        CheckBox cbWednes7 = (CheckBox) findViewById(R.id.Wednes7);
-        CheckBox cbWednes8 = (CheckBox) findViewById(R.id.Wednes8);
-        CheckBox cbWednes9 = (CheckBox) findViewById(R.id.Wednes9);
-        CheckBox cbThurs1 = (CheckBox) findViewById(R.id.Thurs1);
-        CheckBox cbThurs2 = (CheckBox) findViewById(R.id.Thurs2);
-        CheckBox cbThurs3 = (CheckBox) findViewById(R.id.Thurs3);
-        CheckBox cbThurs4 = (CheckBox) findViewById(R.id.Thurs4);
-        CheckBox cbThurs5 = (CheckBox) findViewById(R.id.Thurs5);
-        CheckBox cbThurs6 = (CheckBox) findViewById(R.id.Thurs6);
-        CheckBox cbThurs7 = (CheckBox) findViewById(R.id.Thurs7);
-        CheckBox cbThurs8 = (CheckBox) findViewById(R.id.Thurs8);
-        CheckBox cbThurs9 = (CheckBox) findViewById(R.id.Thurs9);
-        CheckBox cbFri1 = (CheckBox) findViewById(R.id.Fri1);
-        CheckBox cbFri2 = (CheckBox) findViewById(R.id.Fri2);
-        CheckBox cbFri3 = (CheckBox) findViewById(R.id.Fri3);
-        CheckBox cbFri4 = (CheckBox) findViewById(R.id.Fri4);
-        CheckBox cbFri5 = (CheckBox) findViewById(R.id.Fri5);
-        CheckBox cbFri6 = (CheckBox) findViewById(R.id.Fri6);
-        CheckBox cbFri7 = (CheckBox) findViewById(R.id.Fri7);
-        CheckBox cbFri8 = (CheckBox) findViewById(R.id.Fri8);
-        CheckBox cbFri9 = (CheckBox) findViewById(R.id.Fri9);
+        CheckBox Mon1 = (CheckBox) findViewById(R.id.Mon1);
+        Mon1.setOnCheckedChangeListener(this);
+        CheckBox Mon2 = (CheckBox) findViewById(R.id.Mon2);
+        CheckBox Mon3 = (CheckBox) findViewById(R.id.Mon3);
+        CheckBox Mon4 = (CheckBox) findViewById(R.id.Mon4);
+        CheckBox Mon5 = (CheckBox) findViewById(R.id.Mon5);
+        CheckBox Mon6 = (CheckBox) findViewById(R.id.Mon6);
+        CheckBox Mon7 = (CheckBox) findViewById(R.id.Mon7);
+        CheckBox Mon8 = (CheckBox) findViewById(R.id.Mon8);
+        CheckBox Mon9 = (CheckBox) findViewById(R.id.Mon9);
+        CheckBox Tues1 = (CheckBox) findViewById(R.id.Tues1);
+        CheckBox Tues2 = (CheckBox) findViewById(R.id.Tues2);
+        CheckBox Tues3 = (CheckBox) findViewById(R.id.Tues3);
+        CheckBox Tues4 = (CheckBox) findViewById(R.id.Tues4);
+        CheckBox Tues5 = (CheckBox) findViewById(R.id.Tues5);
+        CheckBox Tues6 = (CheckBox) findViewById(R.id.Tues6);
+        CheckBox Tues7 = (CheckBox) findViewById(R.id.Tues7);
+        CheckBox Tues8 = (CheckBox) findViewById(R.id.Tues8);
+        CheckBox Tues9 = (CheckBox) findViewById(R.id.Tues9);
+        CheckBox Wednes = (CheckBox) findViewById(R.id.Wednes1);
+        CheckBox Wednes2 = (CheckBox) findViewById(R.id.Wednes2);
+        CheckBox Wednes3 = (CheckBox) findViewById(R.id.Wednes3);
+        CheckBox Wednes4 = (CheckBox) findViewById(R.id.Wednes4);
+        CheckBox Wednes5 = (CheckBox) findViewById(R.id.Wednes5);
+        CheckBox Wednes6 = (CheckBox) findViewById(R.id.Wednes6);
+        CheckBox Wednes7 = (CheckBox) findViewById(R.id.Wednes7);
+        CheckBox Wednes8 = (CheckBox) findViewById(R.id.Wednes8);
+        CheckBox Wednes9 = (CheckBox) findViewById(R.id.Wednes9);
+        CheckBox Thurs1 = (CheckBox) findViewById(R.id.Thurs1);
+        CheckBox Thurs2 = (CheckBox) findViewById(R.id.Thurs2);
+        CheckBox Thurs3 = (CheckBox) findViewById(R.id.Thurs3);
+        CheckBox Thurs4 = (CheckBox) findViewById(R.id.Thurs4);
+        CheckBox Thurs5 = (CheckBox) findViewById(R.id.Thurs5);
+        CheckBox Thurs6 = (CheckBox) findViewById(R.id.Thurs6);
+        CheckBox Thurs7 = (CheckBox) findViewById(R.id.Thurs7);
+        CheckBox Thurs8 = (CheckBox) findViewById(R.id.Thurs8);
+        CheckBox Thurs9 = (CheckBox) findViewById(R.id.Thurs9);
+        CheckBox Fri1 = (CheckBox) findViewById(R.id.Fri1);
+        CheckBox Fri2 = (CheckBox) findViewById(R.id.Fri2);
+        CheckBox Fri3 = (CheckBox) findViewById(R.id.Fri3);
+        CheckBox Fri4 = (CheckBox) findViewById(R.id.Fri4);
+        CheckBox Fri5 = (CheckBox) findViewById(R.id.Fri5);
+        CheckBox Fri6 = (CheckBox) findViewById(R.id.Fri6);
+        CheckBox Fri7 = (CheckBox) findViewById(R.id.Fri7);
+        CheckBox Fri8 = (CheckBox) findViewById(R.id.Fri8);
+        CheckBox Fri9 = (CheckBox) findViewById(R.id.Fri9);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -67,11 +69,114 @@ public class TtimeActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        CheckBox Mon1 = (CheckBox)findViewById(R.id.Mon1);
+
+        CheckBox Mon2 = (CheckBox)findViewById(R.id.Mon2);
+        Mon2.setOnCheckedChangeListener(this);
+        CheckBox Mon3 = (CheckBox)findViewById(R.id.Mon3);
+        Mon3.setOnCheckedChangeListener(this);
+        CheckBox Mon4 = (CheckBox)findViewById(R.id.Mon4);
+        Mon4.setOnCheckedChangeListener(this);
+        CheckBox Mon5 = (CheckBox)findViewById(R.id.Mon5);
+        Mon5.setOnCheckedChangeListener(this);
+        CheckBox Mon6 = (CheckBox)findViewById(R.id.Mon6);
+        Mon6.setOnCheckedChangeListener(this);
+        CheckBox Mon7 = (CheckBox)findViewById(R.id.Mon7);
+        Mon7.setOnCheckedChangeListener(this);
+        CheckBox Mon8 = (CheckBox)findViewById(R.id.Mon8);
+        Mon8.setOnCheckedChangeListener(this);
+        CheckBox Mon9 = (CheckBox)findViewById(R.id.Mon9);
+        Mon9.setOnCheckedChangeListener(this);
+
+        CheckBox Tues1 = (CheckBox)findViewById(R.id.Tues1);
+        Tues1.setOnCheckedChangeListener(this);
+        CheckBox Tues2 = (CheckBox)findViewById(R.id.Tues2);
+        Tues2.setOnCheckedChangeListener(this);
+        CheckBox Tues3 = (CheckBox)findViewById(R.id.Tues3);
+        Tues3.setOnCheckedChangeListener(this);
+        CheckBox Tues4 = (CheckBox)findViewById(R.id.Tues4);
+        Tues4.setOnCheckedChangeListener(this);
+        CheckBox Tues5 = (CheckBox)findViewById(R.id.Tues5);
+        Tues5.setOnCheckedChangeListener(this);
+        CheckBox Tues6 = (CheckBox)findViewById(R.id.Tues6);
+        Tues6.setOnCheckedChangeListener(this);
+        CheckBox Tues7 = (CheckBox)findViewById(R.id.Tues7);
+        Tues7.setOnCheckedChangeListener(this);
+        CheckBox Tues8 = (CheckBox)findViewById(R.id.Tues8);
+        Tues8.setOnCheckedChangeListener(this);
+        CheckBox Tues9 = (CheckBox)findViewById(R.id.Tues9);
+        Tues9.setOnCheckedChangeListener(this);
+
+        CheckBox Wednes1 = (CheckBox)findViewById(R.id.Wednes1);
+        Wednes1.setOnCheckedChangeListener(this);
+        CheckBox Wednes2 = (CheckBox)findViewById(R.id.Wednes2);
+        Wednes2.setOnCheckedChangeListener(this);
+        CheckBox Wednes3 = (CheckBox)findViewById(R.id.Wednes3);
+        Wednes3.setOnCheckedChangeListener(this);
+        CheckBox Wednes4 = (CheckBox)findViewById(R.id.Wednes4);
+        Wednes4.setOnCheckedChangeListener(this);
+        CheckBox Wednes5 = (CheckBox)findViewById(R.id.Wednes5);
+        Wednes5.setOnCheckedChangeListener(this);
+        CheckBox Wednes6 = (CheckBox)findViewById(R.id.Wednes6);
+        Wednes6.setOnCheckedChangeListener(this);
+        CheckBox Wednes7 = (CheckBox)findViewById(R.id.Wednes7);
+        Wednes7.setOnCheckedChangeListener(this);
+        CheckBox Wednes8 = (CheckBox)findViewById(R.id.Wednes8);
+        Wednes8.setOnCheckedChangeListener(this);
+        CheckBox Wednes9 = (CheckBox)findViewById(R.id.Wednes9);
+        Wednes9.setOnCheckedChangeListener(this);
+
+        CheckBox Thurs1 = (CheckBox)findViewById(R.id.Thurs1);
+        Thurs1.setOnCheckedChangeListener(this);
+        CheckBox Thurs2 = (CheckBox)findViewById(R.id.Thurs2);
+        Thurs2.setOnCheckedChangeListener(this);
+        CheckBox Thurs3 = (CheckBox)findViewById(R.id.Thurs3);
+        Thurs3.setOnCheckedChangeListener(this);
+        CheckBox Thurs4 = (CheckBox)findViewById(R.id.Thurs4);
+        Thurs4.setOnCheckedChangeListener(this);
+        CheckBox Thurs5 = (CheckBox)findViewById(R.id.Thurs5);
+        Thurs5.setOnCheckedChangeListener(this);
+        CheckBox Thurs6 = (CheckBox)findViewById(R.id.Thurs6);
+        Thurs6.setOnCheckedChangeListener(this);
+        CheckBox Thurs7 = (CheckBox)findViewById(R.id.Thurs7);
+        Thurs7.setOnCheckedChangeListener(this);
+        CheckBox Thurs8 = (CheckBox)findViewById(R.id.Thurs8);
+        Thurs8.setOnCheckedChangeListener(this);
+        CheckBox Thurs9 = (CheckBox)findViewById(R.id.Thurs9);
+        Thurs9.setOnCheckedChangeListener(this);
+
+        CheckBox Fri1 = (CheckBox)findViewById(R.id.Fri1);
+        Fri1.setOnCheckedChangeListener(this);
+        CheckBox Fri2 = (CheckBox)findViewById(R.id.Fri2);
+        Fri2.setOnCheckedChangeListener(this);
+        CheckBox Fri3 = (CheckBox)findViewById(R.id.Fri3);
+        Fri3.setOnCheckedChangeListener(this);
+        CheckBox Fri4 = (CheckBox)findViewById(R.id.Fri4);
+        Fri4.setOnCheckedChangeListener(this);
+        CheckBox Fri5 = (CheckBox)findViewById(R.id.Fri5);
+        Fri5.setOnCheckedChangeListener(this);
+        CheckBox Fri6 = (CheckBox)findViewById(R.id.Fri6);
+        Fri6.setOnCheckedChangeListener(this);
+        CheckBox Fri7 = (CheckBox)findViewById(R.id.Fri7);
+        Fri7.setOnCheckedChangeListener(this);
+        CheckBox Fri8 = (CheckBox)findViewById(R.id.Fri8);
+        Fri8.setOnCheckedChangeListener(this);
+        CheckBox Fri9 = (CheckBox)findViewById(R.id.Fri9);
+        Fri9.setOnCheckedChangeListener(this);
+
+        public
+
+
+
+
+
+
 
         int id = item.getItemId();
 
         if( id == R.id.newPost ){
-            Toast.makeText(TtimeActivity.this, "새 글 `등록 버튼을 클릭했습니다.", Toast.LENGTH_SHORT).show();
+            Intent NextBu = new Intent(TtimeActivity.this, ResultActivity.class);
+            TtimeActivity.this.startActivity(NextBu);
             return true;
         }
 
